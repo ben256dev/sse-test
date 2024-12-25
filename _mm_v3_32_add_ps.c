fb303722a9acffe0c4de721c0741df9eb32dabe7
ben256dev <benjamin@ben256.com>
2024-12-25 12:54:18 -0700

    sse test

---
+++ _mm_v3_32_add_ps.c
@@ -0,0 +1,52 @@
+#include <openssl/rand.h>
+#include <butil/butil.h>
+#include <time.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <emmintrin.h>
+
+typedef float v3[3];
+#define V3_SUB 32
+typedef float v3_32[V3_SUB];
+
+int main(void)
+{
+#define VECTOR_COUNT 8388608
+#define VECTORS_BYTES VECTOR_COUNT * sizeof(v3)
+    v3_32* vectors = _mm_malloc(VECTORS_BYTES, sizeof(float) * V3_SUB);
+    if (!vectors)
+        die("_mm_malloc()");
+
+    if (RAND_bytes((unsigned char*)vectors, sizeof(vectors)) != 1)
+        die("RAND_bytes()");
+
+    clock_t start = clock();
+    for (int i = 0; i < (VECTOR_COUNT * 3 / V3_SUB); i++)
+    {
+        __m128 v_1 = _mm_loadu_ps(&vectors[i][0]);
+        __m128 v_2 = _mm_loadu_ps(&vectors[i][4]);
+        __m128 v_3 = _mm_loadu_ps(&vectors[i][8]);
+        __m128 v_4 = _mm_loadu_ps(&vectors[i][12]);
+        __m128 v_5 = _mm_loadu_ps(&vectors[i][16]);
+        __m128 v_6 = _mm_loadu_ps(&vectors[i][20]);
+        __m128 v_7 = _mm_loadu_ps(&vectors[i][24]);
+        __m128 v_8 = _mm_loadu_ps(&vectors[i][28]);
+
+        __m128 v_res_1 = _mm_add_ps(v_1, v_5);
+        __m128 v_res_2 = _mm_add_ps(v_2, v_6);
+        __m128 v_res_3 = _mm_add_ps(v_3, v_7);
+        __m128 v_res_4 = _mm_add_ps(v_4, v_8);
+
+        _mm_storeu_ps(&vectors[i][0],  v_res_1);
+        _mm_storeu_ps(&vectors[i][4],  v_res_2);
+        _mm_storeu_ps(&vectors[i][8],  v_res_3);
+        _mm_storeu_ps(&vectors[i][12], v_res_4);
+    }
+    clock_t end = clock();
+
+    printf("%ld\n", end - start);
+
+    _mm_free(vectors);
+
+    return 0;
+}
---
+++ _mm_v3_8_add_ps.c
@@ -0,0 +1,40 @@
+#include <openssl/rand.h>
+#include <butil/butil.h>
+#include <time.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <emmintrin.h>
+
+typedef float v3[3];
+#define V3_SUB 8
+typedef float v3_8[V3_SUB];
+
+int main(void)
+{
+#define VECTOR_COUNT 8388608
+#define VECTORS_BYTES VECTOR_COUNT * sizeof(v3)
+    v3_8* vectors = _mm_malloc(VECTORS_BYTES, sizeof(float) * V3_SUB);
+    if (!vectors)
+        die("_mm_malloc()");
+
+    if (RAND_bytes((unsigned char*)vectors, sizeof(vectors)) != 1)
+        die("RAND_bytes()");
+
+    clock_t start = clock();
+    for (int i = 0; i < (VECTOR_COUNT * 3 / V3_SUB); i++)
+    {
+        __m128 v_1 = _mm_loadu_ps(&vectors[i][0]);
+        __m128 v_2 = _mm_loadu_ps(&vectors[i][4]);
+
+        __m128 v_res = _mm_add_ps(v_1, v_2);
+
+        _mm_storeu_ps(&vectors[i][0], v_res);
+    }
+    clock_t end = clock();
+
+    printf("%ld\n", end - start);
+
+    _mm_free(vectors);
+
+    return 0;
+}
