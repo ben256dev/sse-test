777206c4e1cf15a27d2c565b5330666a9c02c5ea
ben256dev <benjamin@ben256.com>
2024-12-26 18:59:56 -0700

    better build script and less files

--- build.sh
+++ build.sh
@@ -2,14 +2,44 @@
 
 OPENSSLDIR="/c/Program Files/OpenSSL-Win64/"
 
+COMPILE_FLAGS="-O3 -march=native -ffast-math -funroll-loops -flto -mavx2"
+COMPILE_DEBUG="-g -O0"
+
 compile()
 {
-    #Windows
-    gcc -I"$INCLUDE" -I"${OPENSSLDIR}include" -L"$LIB" -L"${OPENSSLDIR}" \
-        -O3 -march=native -ffast-math -funroll-loops -flto -mavx2 \
-        -o "${c:0:-2}" "$1" -lcrypto -lbutil
+    for c in $@; do
+        #Windows
+        gcc -I"$INCLUDE" -I"${OPENSSLDIR}include" -L"$LIB" -L"${OPENSSLDIR}" \
+            $COMPILE_FLAGS \
+            -o "${c%.*}" "$c" -lcrypto -lbutil
+    done
 }
 
-for c in $(ls | grep '\.c$'); do
-    compile $c
+args=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            echo "usage: $0 [-d | --debug] [-h | --help] file.c ..."
+            exit 0
+            ;;
+        -d|--debug)
+            COMPILE_FLAGS="$COMPILE_DEBUG"
+            ;;
+        -*)
+            echo "Invalid option: $1" >&2
+            echo 'Try "$0 --help" for more information.' >&2
+            exit 1
+            ;;
+        *)
+            args+=("$1")  # Collect non-flag arguments
+            ;;
+    esac
+    shift
 done
+
+if [[ ${#args[@]} -eq 0 ]]; then
+    echo "No source files provided"
+    exit 0
+fi
+
+compile "${args[@]}"
